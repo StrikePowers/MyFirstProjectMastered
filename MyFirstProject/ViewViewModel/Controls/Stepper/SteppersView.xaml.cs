68f8a938f184153fee3aaa11ea0d8462bc304c56
StrikePowers <teherror404@gmail.com>
2022-01-13 15:42:46 -0600

    kinda working

--- MyFirstProject/ViewViewModel/Controls/Stepper/SteppersView.xaml.cs
+++ MyFirstProject/ViewViewModel/Controls/Stepper/SteppersView.xaml.cs
@@ -17,5 +17,12 @@ namespace MyFirstProject.ViewViewModel.Controls.Stepper
             InitializeComponent();
             BindingContext = new SteppersViewModel();
         }
+
+        public void OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            _rotatingLabel.Rotation = value;
+            _displayLabel.Text = string.Format("The Stepper value is {0}", value);
+        }
     }
 }
